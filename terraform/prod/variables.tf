c46e21dc5977014bf298c323af5a2363f53b7db6
BeeHives <112759868+IhebGhazala@users.noreply.github.com>
2023-03-24 12:25:43 +0100

    Update variables.tf

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -1,1 +1,22 @@
+variable "vm_name" {
+  type        = string
+  description = "The name of the virtual machine to be created"
+}
+
+variable "vm_size" {
+  type        = string
+  description = "The size of the virtual machine to be created"
+  default     = "Standard_D2s_v3"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group to deploy the virtual machine into"
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure region where the virtual machine will be deployed"
+  default     = "eastus"
+}
 
