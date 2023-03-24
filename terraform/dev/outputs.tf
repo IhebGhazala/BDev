00c599c532c2c0930e7ed58e03ed342c914e3278
BeeHives <112759868+IhebGhazala@users.noreply.github.com>
2023-03-24 12:21:55 +0100

    Update outputs.tf

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -1,1 +1,20 @@
+output "app_public_ip" {
+  value = azurerm_public_ip.app_public_ip.ip_address
+}
+
+output "vm_username" {
+  value = azurerm_linux_virtual_machine.app_vm.admin_username
+}
+
+output "vm_password" {
+  value = azurerm_linux_virtual_machine.app_vm.admin_password
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.app_rg.name
+}
+
+output "location" {
+  value = azurerm_resource_group.app_rg.location
+}
 
